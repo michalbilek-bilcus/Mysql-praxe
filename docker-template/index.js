698da1b6c4cf9f9873ae9c5255a17f82a2d77b1b
michalbilek-bilcus <112864398+michalbilek-bilcus@users.noreply.github.com>
2023-05-22 13:53:59 +0200

    praxe

--- docker-template/index.js
+++ docker-template/index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-
 const hostname = '127.0.0.1';
 const port = 3000;
 const hasuraAdminSecret = 'myadminsecretkey';
@@ -12,9 +11,10 @@ const server = http.createServer((request, response) => {
     const graphqlEndpoint = 'http://localhost:8080/v1/graphql';
     const query = `
         query GetTestTable {
-            Test_Table {
+            zkouska {
                 id
-                test
+                cotoje
+                jetopravda
             }
         }
     `;
@@ -31,14 +31,48 @@ const server = http.createServer((request, response) => {
         .then(response => response.json())
         .then(data => {
             //práce s databází
-            const results = data.data.Test_Table;
+            const results = data.data.zkouska;
             let output = '<h4>Data:</h4>';
 
             for (let i = 0; i < results.length; i++) {
-                output += `<p>ID: ${results[i].id}</p>`;
-                output += `<p>Test: ${results[i].test}</p>`;
+                output += `<div class="data">`;
+                output += `<p class="text">ID: ${results[i].id}</p>`;
+                output += `<p class="text">cotoje: ${results[i].cotoje}</p>`;
+                output += `<p class="text">jetopravda: ${results[i].jetopravda}</p>`;
+                output += `</div>`;
             }
-            response.end(output);
+
+            response.end(`
+                <html>
+                    <head>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                                background-color: #f5f5f5;
+                                padding: 20px;
+                            }
+                            
+                            h4 {
+                                margin-bottom: 10px;
+                            }
+                            
+                            .data {
+                                background-color: white;
+                                border-radius: 5px;
+                                padding: 10px;
+                                margin-bottom: 10px;
+                            }
+                            
+                            .text {
+                                margin: 5px 0;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        ${output}
+                    </body>
+                </html>
+            `);
         })
         .catch(error => {
             console.error('Error:', error);
