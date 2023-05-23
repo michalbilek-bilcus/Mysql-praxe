55614f061d2faa70ec7caa5e16d3cb4a7e234f24
michalbilek-bilcus <112864398+michalbilek-bilcus@users.noreply.github.com>
2023-05-23 10:06:21 +0200

    praxe

--- docker-template/index.js
+++ docker-template/index.js
@@ -11,10 +11,17 @@ const server = http.createServer((request, response) => {
     const graphqlEndpoint = 'http://localhost:8080/v1/graphql';
     const query = `
         query GetTestTable {
-            zkouska {
+            Test_Table {
                 id
-                cotoje
-                jetopravda
+                test
+            }
+        }
+    `;
+
+    const mutation = `
+        mutation CreateTestEntry($id: Int!, $test: Int!) {
+            insert_Test_Table(objects: {id: $id, test: $test}) {
+                affected_rows
             }
         }
     `;
@@ -31,51 +38,76 @@ const server = http.createServer((request, response) => {
         .then(response => response.json())
         .then(data => {
             //práce s databází
-            const results = data.data.zkouska;
+            const results = data.data.Test_Table;
             let output = '<h4>Data:</h4>';
 
             for (let i = 0; i < results.length; i++) {
                 output += `<div class="data">`;
                 output += `<p class="text">ID: ${results[i].id}</p>`;
-                output += `<p class="text">cotoje: ${results[i].cotoje}</p>`;
-                output += `<p class="text">jetopravda: ${results[i].jetopravda}</p>`;
+                output += `<p class="text">TEST: ${results[i].test}</p>`;
                 output += `</div>`;
             }
 
-            response.end(`
-                <html>
-                    <head>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f5f5f5;
-                                padding: 20px;
-                            }
-                            
-                            h4 {
-                                margin-bottom: 10px;
-                            }
-                            
-                            .data {
-                                background-color: white;
-                                border-radius: 5px;
-                                padding: 10px;
-                                margin-bottom: 10px;
-                            }
-                            
-                            .text {
-                                margin: 5px 0;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        ${output}
-                    </body>
-                </html>
-            `);
+            // Proveď mutaci pro vytvoření nového záznamu
+            fetch(graphqlEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'x-hasura-admin-secret': hasuraAdminSecret,
+                },
+                body: JSON.stringify({
+                    query: mutation,
+                    variables: {
+                        id: 100,
+                        test: 100,
+                    },
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    // Zpracování odpovědi z mutace
+                    console.log('Mutation result:', data);
+
+                    response.end(`
+                        <html>
+                            <head>
+                                <style>
+                                    body {
+                                        font-family: Arial, sans-serif;
+                                        background-color: #f5f5f5;
+                                        padding: 20px;
+                                    }
+                                    
+                                    h4 {
+                                        margin-bottom: 10px;
+                                    }
+                                    
+                                    .data {
+                                        background-color: white;
+                                        border-radius: 5px;
+                                        padding: 10px;
+                                        margin-bottom: 10px;
+                                    }
+                                    
+                                    .text {
+                                        margin: 5px 0;
+                                    }
+                                </style>
+                            </head>
+                            <body>
+                                ${output}
+                            </body>
+                        </html>
+                    `);
+                })
+                .catch(error => {
+                    console.error('Mutation error:', error);
+                    response.end('An error occurred.');
+                });
         })
         .catch(error => {
-            console.error('Error:', error);
+            console.error('Query error:', error);
             response.end('An error occurred.');
         });
 });
